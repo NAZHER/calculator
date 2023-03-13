4ec5b5d1d5c98fd7576248d585f9c593e589daea
NAZHER <naufalnashih@students.amikom.ac.id>
2023-03-13 17:03:34 +0700

    perubahan kedua

---
+++ calculator.cs
@@ -0,0 +1,22 @@
+class calculator
+{
+	static void main (string[] args)
+	{
+		int a = 10;
+		int b =6;
+		
+		console.Writeline("hasil penambahan: {0} + {1} = {2}", a,b, penambahan(a, b)};
+		console.Writeline("hasil pengurangan: {0} + {1} = {2}", a,b, pengurangan(a, b)};
+	
+		console.Writeline{"\nTekan sembarang key untuk keluar");
+		console.Readkey();
+	}
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
